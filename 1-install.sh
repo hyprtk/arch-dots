9d9539a960240f32a5850a635d11199cf64c9aa6
Kori Tk <66495933+hyprtk@users.noreply.github.com>
2026-04-24 09:52:43 +0100

    Update 1-install.sh

--- 1-install.sh
+++ 1-install.sh
@@ -593,6 +593,7 @@ echo ""
 _installSymLink standalone ~/.local/bin ~/arch-dots/standalone/ ~/.local/bin
 _installSymLink oh-my-zsh ~/.oh-my-zsh/oh-my-zsh.sh ~/arch-dots/oh-my-zsh/oh-my-zsh.sh ~/.oh-my-zsh
 echo ""
+rm -R $HOME/dotfiles
 clear
 echo ""
 echo ""
